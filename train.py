afd21523c71814547c19daca0ef581458aea3584
ljjsde <ljjsde@gmail.com>
2025-01-09 19:01:03 +0800

    只是改了保存条件

--- train.py
+++ train.py
@@ -253,7 +253,7 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
                     scalars=scalar_dict
                 )
 
-            if global_step % hps.train.eval_interval == 0:
+            if global_step % 200 == 0:
                 evaluate(hps, net_g, eval_loader, writer_eval)
                 utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch,
                                       os.path.join(hps.model_dir, "G_{}.pth".format(global_step)))
